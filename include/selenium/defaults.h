LabbyLab/zmk-config-aekeynox

chore: define default options in dedicated file (#37)

include/aekeynox/defaults.h
@@ -1,0 +1,35 @@
+// Timing is Key!
+
+#ifndef TAPPING_TERM
+#define TAPPING_TERM 300
+#endif
+
+#ifndef SHORT_TAPPING_TERM
+#define SHORT_TAPPING_TERM 150
+#endif
+
+#ifndef QUICK_TAP
+#define QUICK_TAP 200
+#endif
+
+// Hold-Tap Flavor
+
+#ifdef FOUR_THUMB_KEYS
+  #undef HT_NONE
+  #undef HT_THUMB_TAPS
+  #undef HT_HOME_ROW_MODS
+  #define HT_TWO_THUMB_KEYS
+#elif !defined HT_NONE && !defined HT_THUMB_TAPS && !defined HT_HOME_ROW_MODS && !defined HT_TWO_THUMB_KEYS
+  #define HT_HOME_ROW_MODS
+#elif defined HT_NONE + defined HT_THUMB_TAPS + defined HT_HOME_ROW_MODS + defined HT_TWO_THUMB_KEYS > 1
+  #error "Please select only up to one hold-tap configuration at a time"
+#endif
+
+// Memory
+
+#ifdef LOW_MEMORY_DEVICE
+  #define OMIT_IF_NO_REF /omit-if-no-ref/
+  // #undef memery-hungry options here
+#else
+  #define OMIT_IF_NO_REF
+#endif

cfergeau/zmk-config-selenium

include/selenium/defaults.h
@@ -1,0 +1,35 @@
+// Timing is Key!
+
+#ifndef TAPPING_TERM
+#define TAPPING_TERM 300
+#endif
+
+#ifndef SHORT_TAPPING_TERM
+#define SHORT_TAPPING_TERM 150
+#endif
+
+#ifndef QUICK_TAP
+#define QUICK_TAP 200
+#endif
+
+// Hold-Tap Flavor
+
+#ifdef FORCE_HT_TWO_THUMB_KEYS
+  #undef HT_NONE
+  #undef HT_THUMB_TAPS
+  #undef HT_HOME_ROW_MODS
+  #define HT_TWO_THUMB_KEYS
+#elif !defined HT_NONE && !defined HT_THUMB_TAPS && !defined HT_HOME_ROW_MODS && !defined HT_TWO_THUMB_KEYS
+  #define HT_HOME_ROW_MODS
+#elif defined HT_NONE + defined HT_THUMB_TAPS + defined HT_HOME_ROW_MODS + defined HT_TWO_THUMB_KEYS > 1
+  #error "Please select only up to one hold-tap configuration at a time"
+#endif
+
+// Memory
+
+#ifdef LOW_MEMORY_DEVICE
+  #define OMIT_IF_NO_REF /omit-if-no-ref/
+  // #undef memery-hungry options here
+#else
+  #define OMIT_IF_NO_REF
+#endif